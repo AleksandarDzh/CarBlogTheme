c71ea4fa5983f70fa4fb30ef397230e21f17f07a
Aleksandar Dzhrekos <aleksandardzhrekos@gmail.com>
2023-01-22 22:01:19 +0200

    theme folder added

---
+++ carblog/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php wp_head(); ?> <!-- to load the CSS files -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container" style="margin-bottom: 20px; ">
+	<?php wp_nav_menu( );?>
+</div>
+	<div class="container">
